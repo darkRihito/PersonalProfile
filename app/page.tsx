9dc1ff923e8964536592e655f819310c095047e3
darkRihito <fieshidiq@gmail.com>
2023-08-11 19:08:31 +0700

    darkMode Tailwind

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,27 @@
-import Image from 'next/image'
+"use client"
+import React from "react";
+import Image from "next/image";
+import Nav from "./nav";
 
 export default function Home() {
+  const [darkMode, setDarkMode] = React.useState(true);
+  function toggleDarkMode() {
+    setDarkMode((prevDarkMode) => !prevDarkMode);
+  }
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+    <>
+      <div className={`min-h-screen ${darkMode ? "bg-black" : "bg-white"}`}>
+      <div className="flex justify-center items-center h-full">
+        <button
+          onClick={toggleDarkMode}
+          className={`px-4 py-2 rounded ${
+            darkMode ? "bg-white text-black" : "bg-black text-white"
+          }`}
+        >
+          {darkMode ? "Switch to Light Mode" : "Switch to Dark Mode"}
+        </button>
+      </div>
+    </div>
+    </>
+  );
 }
