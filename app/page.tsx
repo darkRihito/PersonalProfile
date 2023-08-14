48896d114bb81d7bf949e7415fd22e8a238e5314
darkRihito <fieshidiq@gmail.com>
2023-08-14 23:24:00 +0700

    layout3

--- app/page.tsx
+++ app/page.tsx
@@ -23,10 +23,10 @@ export default function Home() {
         </div>
       </div> */}
 
-      <div className="max-w-[120rem] mx-auto">
+      <div className="max-w-[120rem] mx-auto mb-16">
         <div
           id="head"
-          className="border-2 border-slate-300 w-full h-52 flex items-start justify-center md:justify-end pt-12 px-12"
+          className="border-2 border-slate-300 bg-slate-300 w-full h-52 flex items-start justify-center md:justify-end pt-12 px-8 md:px-12"
         >
           <div id="navigationMenu" className="">
             <ul className="flex flex-wrap items-center justify-center text-xl font-semibold text-gray-900 dark:text-white gap-8 md:gap-12">
@@ -49,11 +49,11 @@ export default function Home() {
           </div>
         </div>
 
-        <div className="flex flex-col md:flex-row px-4 md:px-14 gap-4 md:gap-8 relative">
-          <div id="aside" className="border-2 border-yellow-100 w-full max-w-sm mx-auto relative -top-10 h-[32rem]">
+        <div className="flex flex-col lg:flex-row px-4 md:px-14 gap-4 md:gap-8 relative">
+          <div id="aside" className="border-2 border-yellow-100 bg-yellow-100 w-full max-w-xl lg:max-w-xs relative -top-10 h-[18rem] lg:h-[32rem]">
             <div id="profile">profile</div>
           </div>
-          <div id="content" className="border-2 border-red-200 flex-grow">
+          <div id="content" className="border-2 border-red-200 bg-red-200 flex-grow h-full min-h-[36rem]">
             content
           </div>
         </div>
