21ae34dcbcb9e2c53044a68ed84eec02db432223
darkRihito <fieshidiq@gmail.com>
2023-08-18 23:40:36 +0700

    commit

--- app/page.tsx
+++ app/page.tsx
@@ -314,6 +314,24 @@ export default function Home() {
                           <div className="">Certification</div>
                         </li>
                       </a>
+                      <a href="#" className="hover:underline ">
+                        <li className="flex items-center space-x-2 justify-center text-base">
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="30"
+                            height="30"
+                            viewBox="0 0 24 24"
+                            fill="none"
+                            stroke="#000000"
+                            strokeWidth="1.5"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                          >
+                            <polygon points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2"></polygon>
+                          </svg>
+                          <div className="">Blog</div>
+                        </li>
+                      </a>
                     </ul>
                   </div>
                 </div>
